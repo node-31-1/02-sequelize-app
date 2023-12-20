f610cfac96f36ce65b83eb12d19b9ca66d8b915b
Andres Mendoza <andres.david.mm@hotmail.com>
2023-12-19 23:01:04 -0500

    first commit

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection'); 
+   // En Mayúsculas y singular      // en minúsculas y singular
+const User = sequelize.define('user', {
+    // Definimos las columnas aquí
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    birthday: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+    },
+});
+
+module.exports = User;
+
