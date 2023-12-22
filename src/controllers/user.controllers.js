8d55840cccbe4b5bde11caa01357ee0d0a16088d
Andres Mendoza <andres.david.mm@hotmail.com>
2023-12-21 22:58:58 -0500

    added get one, delete and update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,7 +17,38 @@ const create = catchError(async(req, res) => {
     return res.status(201).json(user);
 });
 
+// /users/3
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params;
+    // const user = await User.findOne({ where: { id: 2 } })
+    const user = await User.findByPk(id);
+    return res.json(user);
+});
+
+// /users/1
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: { id: id } });
+    return res.sendStatus(204);
+})
+
+// /users/1
+const update = catchError(async(req, res) => {
+    const { id } = req.params;
+    const { name, email, phone, birthday } = req.body;
+    const user = await User.update({
+        name: name,
+        email: email,
+        phone: phone,
+        birthday: birthday,
+    }, { where: { id: id }, returning: true });
+    return res.json(user);
+});
+
 module.exports = {
     getAll,
-    create
+    create,
+    getOne,
+    remove,
+    update,
 }
--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,4 @@
-const { getAll, create } = require('../controllers/user.controllers');
+const { getAll, create, getOne, remove, update } = require('../controllers/user.controllers');
 const express = require('express');
 
 const userRouter = express.Router();
@@ -6,5 +6,10 @@ const userRouter = express.Router();
 userRouter.route("/users")
 	.get(getAll)
 	.post(create)
+
+userRouter.route("/users/:id")
+	.get(getOne)
+	.delete(remove)
+	.put(update);
 
 module.exports = userRouter;
